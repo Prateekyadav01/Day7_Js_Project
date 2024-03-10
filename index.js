5bee4418bf70ffbfaf2eca01c5c6a5fe11012388
PrateekTechmatys <prateeky562@gmail.com>
2024-03-10 22:09:55 +0530

    adding

--- index.js
+++ index.js
@@ -1,49 +1,72 @@
 
+const Products =[
+    {
+        id: 1,
+        name: "Product-1",
+        price:100
+    },
+    {
+        id: 2,
+        name: "Product-2",
+        price:200
+    },
+    {
+        id: 3,
+        name: "Product-3",
+        price:300
+    }
+]
 
-console.log(Products);
 
-let main = document.getElementById("main");
+let productList = document.getElementById("product");
 
-// Products.forEach(function(product) {
-//     let div = document.createElement("div");
-//     let increment = document.createElement("button");
-//     let decrement = document.createElement("button");
-//     let li = document.createElement("span");
-//     let li1 = document.createElement("span");
-//     let li2 = document.createElement("span");
-//     li.textContent = product.id;
-//     li1.textContent = product.name;
-//     li2.textContent = product.price;
-//     div.appendChild(li);
-//     div.appendChild(li1);
-//     div.appendChild(li2);
-//     increment.style.width="100px";
-//     increment.style.height="100px";
-//     increment.innerHTML = "+";
-//     decrement.style.width="100px";
-//     decrement.style.height="100px";
-//     decrement.innerHTML = "-";
-//     div.appendChild(increment);
-//     div.appendChild(decrement);
-//     main.appendChild(div);
-// });
+Products.forEach(function(product) {
+    let div = document.createElement("div");
+    let paraId = document.createElement("p");
+    let paraName = document.createElement("p");
+    let paraPrice = document.createElement("p");
 
-const ProductsItem = [...new Set(Products.map((item)=>{
-    return item
-}))]
 
-let i=0;
-document.getElementById('root').innerHTML=ProductsItem.map((item)=>{
+    let buttonDiv = document.createElement("div");
+    let increment = document.createElement("button");
+    let count = document.createElement("p");
+    let decrement = document.createElement("button");
 
-    var{ id , name , price}= item;
-    return(
-        `<div class='box'>
-        <div class='img-box'>
-         <p class='number'>${id}</p>
-         <p>${name}</p>
-         <p>${price}.00</p>`+
-         "<button onclick='+("+(i++)+")'>+</button>"+
-         `</div>
-         </div>`
-    )
-}).join('');
+    paraId.textContent =   product.id;
+    paraName.textContent =  product.name;
+    paraPrice.textContent =  product.price;
+
+    increment.textContent = "+";
+    count.textContent = 0;
+    decrement.textContent = "-";
+
+    increment.style.width = "30px";
+    increment.style.height = "30px";
+    decrement.style.width = "30px";
+    decrement.style.height = "30px";
+
+
+    buttonDiv.appendChild(increment);
+    buttonDiv.appendChild(count);
+    buttonDiv.appendChild(decrement);
+    buttonDiv.classList.add('button-js');
+
+    div.appendChild(paraId);
+    div.appendChild(paraName);
+    div.appendChild(paraPrice);
+    div.appendChild(buttonDiv);
+    div.classList.add('product_js');
+
+    productList.appendChild(div);
+
+
+        increment.addEventListener("click", function() {
+        count.textContent = parseInt(count.textContent) + 1;
+    });
+    decrement.addEventListener("click", function() {
+        if(count.textContent>0){
+            count.textContent = parseInt(count.textContent) - 1;
+        }
+    });
+    
+});
